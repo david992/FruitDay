20b95df68de08200462664dce74e570be20919a2
david992 <24604567@qq.com>
2020-05-08 22:34:37 +0800

    js修改

--- index/urls.py
+++ index/urls.py
@@ -4,11 +4,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-  url(r'^login/$', login_views, name='login'),
+  path('login/', login_views, name='login'),
   # url(r'^register/$', register_views, name='reg'),
-  url(r'^check_uphone/$', check_uphone_views),
+  path('check_uphone/', check_uphone_views),
   url(r'^$', index_views),
-  url(r'^check_login/$', check_login_views),
+  path('check_login/', check_login_views),
   url(r'^logout/$', logout_views),
   url(r'^test/$', test_views),
   url(r'^load_type_goods/$', type_goods_views),
